8d8ad829bdbf0792e52141eb315d884a249f160a
JiangWenJie <1159735488@qq.com>
2018-01-29 09:30:24 +0800

    姜文杰
    RandomAccess.java接口翻译

--- java/util/ArrayList.java
+++ java/util/ArrayList.java
@@ -38,7 +38,7 @@ import java.util.function.UnaryOperator;
  * used internally to store the list.  (This class is roughly equivalent to
  * <tt>Vector</tt>, except that it is unsynchronized.)
  *
- * 实现了List 接口的大小可变数组。实现了所有可选列表操作，
+ * 实现了List 接口的大小可变数组。实现了所有可选的列表操作，
  * 并允许包括 null 在内的所有元素。
  * 除了实现 List 接口外，此类还提供一些方法来操作内部用来存储列表的数组的大小。
  * （此类大致上等同于 Vector 类，除了此类是不同步的。）
